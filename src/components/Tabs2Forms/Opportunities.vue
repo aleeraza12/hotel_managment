99998d2d7ef45eb375befbbac59d2101a49d2820
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-02-05 11:03:23 +0500

    4th section

--- src/components/Tabs2Forms/Opportunities.vue
+++ src/components/Tabs2Forms/Opportunities.vue
@@ -443,137 +443,94 @@
         </v-btn>
       </div>
     </v-card>
-<!-- 4th card -->
-<v-card
-class="my-auto mt-5"
-    max-width="554"
-    outlined>
-    <div>
-      <v-icon>
-        mdi-
-      </v-icon>
-      <span>Add group spaces</span>
-       <div>
-      Feb 2,2022 . 1 minute to do
-    </div>
-    <div>{{text}}</div>
-    <v-divider></v-divider>
-     <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div> <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div> <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div> <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-          
-        </v-col>
-      </v-row>
-    </div> <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div> <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div>
-    <div class="pl-8 pr-8">
-      <v-row class="d-flex">
-        <v-col sm="4" md="4">
-          <v-checkbox
-            label="red"
-            color="red"
-            value="red"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-        <v-col sm="4" md="4">
-         
-        </v-col>
-      </v-row>
-    </div>
-    <div>
-       <v-select
-          :items="items"
-          label="Fee Type"
-          outlined
-        ></v-select>
-    </div>
+     <!-- 4th card -->
+    <v-card
+      class="my-auto mt-5 pa-5"
+      max-width="554"
+      outlined
+    >
       <div>
-      <v-btn outlined rounded >
+        <v-icon color="primary" dense small >
+         mdi-account
+        </v-icon>
+        <span class="card-title ml-1 mr-5">
+          Add group spaces
+        </span>
+      </div>
+      <div class="date ml-5">
+        Feb 2,2022 . 1 minute to do
+      </div>
+      <div class="card-body-text ma-3">
+        {{text}}
+      </div>
+      <v-divider></v-divider>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div  class="d-flex ml-3 ">
+        <v-checkbox
+          label="red"
+          color="primary"
+          value="red"
+          hide-details    
+        ></v-checkbox>
+      </div>
+      <div class="mt-2 ml-10" style="width:60%">
+        <v-select class="mb-0" dense small :items="items" label="Coste" outlined></v-select>
+      </div>
+      <div class="ml-5">
+        <v-btn rounded outlined dense small color="primary">
           Save
-      </v-btn><span>I don't have any of these</span>
-    </div>
-    </div>
-</v-card>
-
-<!-- 5th card -->
+        </v-btn>
+        <span class="ml-5" style="color:grey">I don't have any of these</span>
+      </div>
+    </v-card>
+      <!-- 5th card -->
 
 <v-card
 class="my-auto mt-5"
