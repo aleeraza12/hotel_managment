cad2e65aa3607d51c4d8d814f24931712493bf13
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-02-08 23:51:15 +0500

    rooma

--- src/components/Tabs2Forms/RoomsSpacesForm.vue
+++ src/components/Tabs2Forms/RoomsSpacesForm.vue
@@ -1,30 +1,35 @@
 <template>
     <div>
-        <div>
+        <div class="heading mt-2 mb-5">
             Rooms and spaces
         </div>
-        <div>Let guests know about the property's rooms spaces</div>
+        <div class="sub-heading mt-2 mb-5">Let guests know about the property's rooms spaces</div>
          <v-expansion-panels>
     <v-expansion-panel
     >
       <v-expansion-panel-header>
-   Property details
+       <div class="card-title"> Property details </div>
+      
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <div>
-        <v-select class="mb-0" dense small :items="items" label="Coste" outlined></v-select>
+        <v-select class="mb-0 select-dropdown" dense small :items="items" label="Coste" outlined></v-select>
          </div>
-         <div>Living area</div>
-         <div class="d-flex">
-                <div class="d-flex">
-        <v-select class="mb-0" dense small :items="items1" label="Coste" outlined></v-select>
+         <div class="card-title">Living area</div>
+             <div class="d-flex">
+           <div >
+        <v-select style="width:50%" class="mb-0" dense small :items="items" label="Coste" outlined></v-select>
+        </div>
+        <div style="width:100%">
          <v-text-field
+         style="margin-left:-5rem"
         outlined
-        label='2000'
+        dense
+        label='213323124124124'
         class="text-feild"
         ></v-text-field>
         </div>
-        </div>
+         </div>
         <div>
             <v-checkbox label="Senior adults only community"></v-checkbox>
             <v-checkbox label="Long term renters Welcome"></v-checkbox>
@@ -34,10 +39,10 @@
     </v-expansion-panel>
     <v-expansion-panel>
         <v-expansion-panel-header>
-            Accessibilty
+           <div class="card-title">Accessibilty</div> 
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-            <div>Is your property wheelchair accessible?</div>
+            <div class="sub-heading">Is your property wheelchair accessible?</div>
                  <v-radio-group v-model="column" column dense>
         <v-radio label="Yes, it is accessible" value="radio-1"></v-radio>
         <v-radio label="No it is not accessible" value="radio-1"></v-radio>
@@ -46,97 +51,97 @@
     </v-expansion-panel>
      <v-expansion-panel>
         <v-expansion-panel-header>
-            Sleeping arrangements
+          <div class="card-title">  Sleeping arrangements</div> 
         </v-expansion-panel-header>
         <v-expansion-panel-content>
             <div>  
-            <div>Sharing the bed types in each room will help guests plan their stay</div>
+            <div class="sub-heading">Sharing the bed types in each room will help guests plan their stay</div>
             <div class="d-flex">
-                <div>Basement Queen</div>
-                <div class="ml-10">Edit</div>
+                <div class="card-title mb-3 mt-3">Basement Queen</div>
+                <div style="margin-left:12rem;color:blue" class="mt-3" >Edit</div>
                 </div>
-                <div>
+                <div class="sub-heading">
                     Queen bed
                 </div>
-                <v-chip>Queen(1)</v-chip>
+                <v-chip class="mt-2 mb-2" dense small>Queen(1)</v-chip>
                 </div>
             <div class="d-flex">
-                <div>Master Bedroom</div>
-                <div class="ml-10">Edit</div>
+                <div class="card-title mt-3 mb-3">Master Bedroom</div>
+                <div style="margin-left:12rem;color:blue" class="mt-3" >Edit</div>
                 </div>
-                <div>
+                <div class="sub-heading">
                     Master bach and walk in closet
                 </div>
-                <v-chip>king(1)</v-chip>
+                <v-chip class="mt-2 mb-2" small dense>king(1)</v-chip>
                 <div class="d-flex">
-                <div>Queen 2</div>
-                <div class="ml-10">Edit</div>
+                <div class="card-title mt-3 mb-3">Queen 2</div>
+                <div style="margin-left:15rem;color:blue" class="mb-2 mt-2">Edit</div>
                 </div>
-                <div>
+                <div class="sub-heading">
                     Queen bed
                 </div>
-                <v-chip>Queen(2)</v-chip>
+                <v-chip small dense class="mt-2 mb-2">Queen(2)</v-chip>
+                <div class="align-start justify-start d-flex">
+                <v-btn class="mt-5 mb-5" color="primary" small dense rounded outlined>Add Bedroom</v-btn>
+                </div> 
+                <div class="mt-5 mb-2" style="color:blue;font-size:13px">+Add  additional Bedroom details</div>
                 <div>
-                <v-btn color="primary" rounded outlined>Add Bedroom</v-btn>
-                </div>
-                <div>+Add  additional Bedroom details</div>
-                <div>
-                <div>
+                <div class="card-title mt-5 mb-2">
                 Common Areas    
                 </div>    
-                <div>
+                <div class="sub-heading">
                     Shared spaces with beds or couches? Add them here
                 </div>
                 </div>   
                 <div>
-                     <v-btn color="primary" rounded outlined>Add common area</v-btn>
+                     <v-btn class="mt-5 mb-5" color="primary" small dense  rounded outlined>Add common area</v-btn>
                 </div> 
         </v-expansion-panel-content>
          </v-expansion-panel>
         <v-expansion-panel>
             <v-expansion-panel-header>
-                Bathrooms
+             <div class="card-title"> Bathrooms </div>   
             </v-expansion-panel-header>
             <v-expansion-panel-content>
                 <div>
-                    <div>
+                    <div class="sub-heading">
                         Adding bathroom details will help guests determine if your property will be a good fit for their stay 
                     </div>
                     <div class="d-flex">
-                <div>Main level</div>
-                <div class="ml-10">Edit</div>
+                <div class="mt-5 mb-2 card-title">Main level</div>
+                <div style="margin-left:12rem;color:blue" class="mt-5">Edit</div>
                 </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5">
                    Off of Kitchen
                 </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5" >
                     Toilet
                 </div>
                  <div class="d-flex">
-                <div>Master Bath</div>
-                <div class="ml-10">Edit</div>
+                <div class="mt-5 mb-2 card-title">Master Bath</div>
+                <div  style="margin-left:12rem;color:blue" class="mt-5">Edit</div>
                  </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5">
                    Master Bedroom
                 </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5">
                     Shower,Toilet
                 </div>
                  <div class="d-flex">
-                <div>Shared Bathroom</div>
-                <div class="ml-10">Edit</div>
+                <div class="card-title mb-5 mt-5">Shared Bathroom</div>
+                <div style="margin-left:12rem;color:blue" class="mt-5">Edit</div>
                  </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5">
                    Top of Stars
                 </div>
-                <div>
+                <div class="sub-heading mb-5 mt-5">
                     Tub,Combination tub/shower,Shower, Toilet
                 </div>
                 </div>
                 <div>
-                     <v-btn color="primary" rounded outlined>Add bathroom</v-btn>
+                     <v-btn class="mb-5 mt-5" color="primary" small dense rounded outlined>Add bathroom</v-btn>
                 </div>
-                <div>+Add additional bathroom details</div>
+                <div  class="mt-5 mb-2" style="color:blue;font-size:13px">+Add additional bathroom details</div>
             </v-expansion-panel-content>
     </v-expansion-panel>
     <v-expansion-panel>
@@ -185,3 +190,30 @@
     },
   }
 </script>
+<style scoped>
+.heading {
+  font-size:16px;
+  font-weight: bold;
+}
+.sub-heading {
+  font-size:14px;
+}
+.text-feild{
+  width: 50%;
+}
+.card-title{
+  font-size:14px;
+  font-weight: bold;
+}
+.date {
+  font-size:12px;
+  color: grey;
+}
+.card-body-text {
+  font-size:12px;
+  text-align: justify;
+}
+.select-dropdown {
+  width: 30% !important;
+}
+</style>
