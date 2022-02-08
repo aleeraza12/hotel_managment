9ea08323b863258fc04eb3d45618a61a8cb75b28
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-02-08 22:39:01 +0500

    description form

--- src/components/Tabs2Forms/DescriptionForm.vue
+++ src/components/Tabs2Forms/DescriptionForm.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <h3>Property description</h3>
-        <div>
-            <v-icon>mdi-information</v-icon>
-            <span>{{text}}</span>
+        <div class="heading mt-5">Property description</div>
+        <div class="d-flex">
+            <v-icon color="primary mr-5" >mdi-information</v-icon>
+            <div class="sub-heading ma-3" style="width:50%;text-align:justify">{{text}}</div>
         </div>
+        <div class="card-title mt-5 mb-5" style="color:blue">view partners update</div>
         <div>
            <div>
            <v-text-field
+           dense
                outlined
             :label='text'
             class="text-feild"
@@ -15,6 +17,7 @@
           </div>
           <div>
            <v-text-field
+           dense
                outlined
             :label='text'
             class="text-feild"
@@ -23,23 +26,24 @@
           <div>
            <v-textarea
                outlined
-            :label='text'
+            :label='Description'
             class="text-feild"
           ></v-textarea>
           </div>
           </div>
           <div>
-              <h3> Are you a professional host or private host?</h3>
-                <div>
+              <div class="card-title"> Are you a professional host or private host?</div>
+                <div class="sub-heading">
       <v-radio-group v-model="column" column dense>
         <v-radio :label="text" value="radio-1"></v-radio>
         <v-radio :label="text" value="radio-2"></v-radio>
       </v-radio-group>
     </div>
     <div>
-        <h2>Regulatory</h2>
+        <div class="heading mt-5 mb-5">Regulatory</div>
         <div>
         <v-text-field
+        dense
         outlined
         :label='text'
         class="text-feild"
@@ -47,29 +51,30 @@
         </div>
     </div>
     <div>
-        <h3>
+        <div class="heading mb-5 mt-5">
             Owner story
-            <div>{{text}}</div>
-        </h3>
+        </div>
+         <div class="sub-heading mb-10 mt-10" style="width:50%;text-align:justify">{{text}}</div>
     </div>
     <div>
-        <v-icon>mdi-account-circle</v-icon><v-btn rounded outlined>Upload photo</v-btn>
+        <v-icon color="grey" size="100">mdi-account-circle</v-icon>
+        <v-btn rounded outlined color="primary" class="mt-10 ml-5">Upload photo</v-btn>
     </div>
-    <div>
+    <div class="mt-5">
          <v-textarea
                outlined
             label='About you'
             class="text-feild"
           ></v-textarea>
     </div>
-    <div>
+    <div class="mt-5">
          <v-textarea
                outlined
             label='Why this property'
             class="text-feild"
           ></v-textarea>
     </div>
-    <div>
+    <div class="mt-5">
          <v-textarea
                outlined
             label='Unique benefits'
@@ -90,3 +95,30 @@ export default {
     },
 }
 </script>
+<style scoped>
+.heading {
+  font-size:16px;
+  font-weight: bold;
+}
+.sub-heading {
+  font-size:14px;
+}
+.text-feild{
+  width: 50%;
+}
+.card-title{
+  font-size:14px;
+  font-weight: bold;
+}
+.date {
+  font-size:12px;
+  color: grey;
+}
+.card-body-text {
+  font-size:12px;
+  text-align: justify;
+}
+.select-dropdown {
+  width: 30% !important;
+}
+</style>
