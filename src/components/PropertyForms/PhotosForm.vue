1ceccc9cf0fab2321002caab3250447d6da1c2c2
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-02-20 20:04:42 +0500

    photos form

--- src/components/PropertyForms/PhotosForm.vue
+++ src/components/PropertyForms/PhotosForm.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <div class="d-flex">
-      <div>Photos</div>
+<div>
+  <v-card class="elevation-5">
+    <v-card-title>
+       <div>Photos</div>
       <v-spacer></v-spacer>
-      <div><v-btn color="primary" rounded small class="mb-1">Add</v-btn></div>
-    </div>
-    <div>
+      <div> <v-btn rounded outlined dense small color="#2196F3">
+         Add
+        </v-btn></div>
+    </v-card-title>
+    <v-card-subtitle>
+      <div>
       Your first photo will be the photo travelers see in search results
     </div>
+    </v-card-subtitle>
+
+    <v-card-text>
+  <div>
+
      <v-container class="pa-0">
     <v-row class="pa-0">
       <v-col cols="8">
@@ -15,13 +24,16 @@
         <v-col v-for="(post, i) in posts" :key="i" cols="2">
         <base-card :href="post.href" color="grey lighten-2" height="88" tag="a">
           <v-img
+            style="border: 1px solid grey;
+    border-radius: 5px;"
             v-if="post.src"
             :src="require(`/public/instagram/${post.src}`)"
             height="100%"
           />
         </base-card>
       </v-col>
-        <div style="height:90px;width:90px;background-color:light-gray;border:1px solid blue" class="ml-3 mt-3"><v-icon class="mt-2 text-center align-center justify-center mx-auto">mdi-plus</v-icon><span>Add photos</span></div>
+        <div style="height:90px;width:100px;background-color:light-gray;border:1px solid grey" class="ml-3 mt-3 pt-7">
+          <v-icon class="ml-3" small color="#2196F3">mdi-plus-circle-outline</v-icon><span style="font-size:12px">Add photos</span></div>
     </v-row>
     </v-col>
     <v-col cols="4">
@@ -29,8 +41,12 @@
           class="pa-12"
           color="grey lighten-3"
           height="210"
+          style="border: 1px solid grey;
+    border-radius: 10px;"
         >
-         <v-btn rounded>Rotate</v-btn>
+         <v-btn style="margin-top:7.5rem ;margin-left:11rem" rounded outlined dense small color="#2196F3">
+          Rotate
+        </v-btn>
           <div></div>
         </v-sheet>
          <v-text-field
@@ -38,13 +54,16 @@
             label="Photo caption"
             placeholder="Placeholder"
             outlined
+            dense
           ></v-text-field>
-          <div>Move to archive</div>
-          <div>Delete</div>
+          <div class="ma-2">Move to archive</div>
+          <div class="ma-2">Delete</div>
              </v-col>
     </v-row>
      </v-container>
-    <div>
+     <v-card class="elevation-5 mt-5">
+       <v-card-text>
+   <div>
       <h4>Tips</h4>
       Upload clear ,bright photos that show off your rooms and amenities, it may
       take up to 10 minutes for your listing to update with changes
@@ -58,7 +77,13 @@
       We will sometimes edit or delete individual photos if they break our
       guidelines
     </div>
+       </v-card-text>
+     </v-card>
+ 
   </div>
+  </v-card-text>
+  </v-card>
+</div>
 </template>
 <script>
 export default {
